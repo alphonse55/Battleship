37e98b893594af2cc8105c1dd7100241082fd8c4
alphonse55 <alphonse.raynaud@icloud.com>
2021-08-14 11:59:10 +0200

    renamed position to coordinates

--- Ship.java
+++ Ship.java
@@ -4,7 +4,7 @@ public class Ship {
     int y;
     int orientation;
     String name;
-    int[][] ship_positions;
+    int[][] ship_coordinates;
 
     public Ship(boolean[][] mat, int len_ship, int x, int y, int orientation, String name){
         this.x = x;
@@ -12,20 +12,20 @@ public class Ship {
         this.orientation = orientation;
         this.name = name;
 
-        ship_positions = new int[len_ship][2];
+        ship_coordinates = new int[len_ship][2];
 
         if (orientation == 0){
             for (int i = x; i < x + len_ship; i++){
                 mat[y][i] = true;
-                int[] position = {i, y};
-                ship_positions[i - x] = position;
+                int[] coordinates = {i, y};
+                ship_coordinates[i - x] = coordinates;
             }
         }
         else if (orientation == 1){
             for (int i = y; i < y + len_ship; i++){
                 mat[i][x] = true;  
-                int[] position = {x, i};
-                ship_positions[i - y] = position;
+                int[] coordinates = {x, i};
+                ship_coordinates[i - y] = coordinates;
             }
         }
     }
