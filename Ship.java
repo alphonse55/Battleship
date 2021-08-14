6eddc529f766a8beb3ada7800f0779ed6a0ad7a3
alphonse55 <alphonse.raynaud@icloud.com>
2021-08-14 11:55:52 +0200

    added ship_position array to Ship class

--- Ship.java
+++ Ship.java
@@ -4,6 +4,7 @@ public class Ship {
     int y;
     int orientation;
     String name;
+    int[][] ship_positions;
 
     public Ship(boolean[][] mat, int len_ship, int x, int y, int orientation, String name){
         this.x = x;
@@ -11,13 +12,21 @@ public class Ship {
         this.orientation = orientation;
         this.name = name;
 
+        ship_positions = new int[len_ship][2];
+
         if (orientation == 0){
-            for (int i = x; i < x + len_ship; i++)
+            for (int i = x; i < x + len_ship; i++){
                 mat[y][i] = true;
+                int[] position = {i, y};
+                ship_positions[i - x] = position;
+            }
         }
         else if (orientation == 1){
-            for (int i = y; i < y + len_ship; i++)
+            for (int i = y; i < y + len_ship; i++){
                 mat[i][x] = true;  
+                int[] position = {x, i};
+                ship_positions[i - y] = position;
+            }
         }
     }
 }
